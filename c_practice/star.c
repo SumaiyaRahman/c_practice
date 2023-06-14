4ee6ad6584b0212ec0a2bb794299f2b37dfb592a
sumaiya <sumaiya.rahman.cse@gmail.com>
2023-06-14 23:56:38 +0600

    Update star.c
    indentation is fixed

--- c_practice/star.c
+++ c_practice/star.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 int main()
 {
-int i;
-for(i=0;i<10;i++)
-{//for(i=0;i<2;i++)
-{
-printf("*\n");}
+    int i;
+    for(i=0; i<10; i++)
+    {
+        //for(i=0;i<2;i++)
+        {
+            printf("*\n");
+        }
 
-}
+    }
 
-return 0;
+    return 0;
 }
